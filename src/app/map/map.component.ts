6166c17d82d9c3fc72a30a275322ecc6b70a2906
ghisloufou <ghisloufer@hotmail.fr>
2021-07-12 10:00:01 +0200

    added traveler svg and removed backend code

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -12,7 +12,6 @@ export class MapComponent implements OnInit {
 
   @Input() set map(value: TreasureMap) {
     this._map = value;
-    this.fillMap();
   }
   get map(): TreasureMap {
     return this._map;
@@ -21,50 +20,11 @@ export class MapComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    // Get data then fill the map
-    // this.ds.dataObservable().subscribe((treasureMap) => {
-    //   this.map = treasureMap;
-    //   this.fillMap();
-    //   this.setCell({ value: 1, type: 'Mountain', x: 2, y: 1 });
-    //   this.setCell({ value: 2, type: 'Treasure', x: 3, y: 2 });
-    //   console.table(this.matrix);
-    // });
   }
 
   emitHoveredCell(cell: CellData) {
     this.hoveredCellEvent.emit(cell);
   }
-
-  /**
-   * Fill the map with the height and width
-   */
-  fillMap() {
-    // if (!this.map) {
-    //   this.map = { width: 10, height: 4 };
-    // }
-    // this.matrix = Array(this.map.height)
-    //   .fill(null)
-    //   .map((a, i) => {
-    //     return Array(this.map.width)
-    //       .fill(null)
-    //       .map((b, j) => {
-    //         return { value: 1, type: 'Grass', x: i + 1, y: j + 1 };
-    //       });
-    //   });
-    if (this.map) {
-      this.map.matrix = Array(this.map.height)
-        .fill(null)
-        .map((a, i) => {
-          return Array(this.map.width)
-            .fill(null)
-            .map((b, j) => {
-              return { value: 1, type: 'Grass', x: i + 1, y: j + 1 };
-            });
-        });
-    }
-  }
-
-  addItemToArray() {}
 
   /**
    * Set a value in the map at certain coordinates
